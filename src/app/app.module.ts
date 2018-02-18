068cbcb5669a478c1e5e77e18dcb185d4d87131a
Adrian Meza <adrianme213@gmail.com>
2018-02-18 12:32:57 -0800

    [feat] - use property binding to give parent component control over details component - use @Input decorator to make hero property available for binding by the parent component - refactor data access to a service - use Angular dependency injection to inject into component - give HeroService getHeroes data method - use RxJS Observable library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,27 @@ import { FormsModule } from '@angular/forms'; // [(ngModel)] is found here
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroService,
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
